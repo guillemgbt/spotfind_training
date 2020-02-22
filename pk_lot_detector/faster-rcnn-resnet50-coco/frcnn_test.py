7f22e6e3dc4dc6d39f4303bd0062dd28f57ed17d
Guillem Budia Tirado <guillem.budia@monkingme.com>
2020-02-22 13:09:46 -0700

    faster rcnn resnet50 coco ignoring training big files

---
+++ pk_lot_detector/faster-rcnn-resnet50-coco/frcnn_test.py
@@ -0,0 +1,48 @@
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+from matplotlib import pyplot as plt
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+
+
+class ParkingLotDetector(object):
+    def __init__(self):
+        PATH_TO_MODEL = 'fine_tuned_model/frozen_inference_graph.pb'
+        self.detection_graph = tf.Graph()
+        with self.detection_graph.as_default():
+            od_graph_def = tf.GraphDef()
+            # Works up to here.
+            with tf.gfile.GFile(PATH_TO_MODEL, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+            self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
+            self.d_boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
+            self.d_scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
+            self.d_classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
+            self.num_d = self.detection_graph.get_tensor_by_name('num_detections:0')
+        self.sess = tf.Session(graph=self.detection_graph)
+
+    def get_classification(self, img):
+        # Bounding Box Detection.
+        with self.detection_graph.as_default():
+            # Expand dimension since the model expects image to have shape [1, None, None, 3].
+            img_expanded = np.expand_dims(img, axis=0)
+            (boxes, scores, classes, num) = self.sess.run(
+                [self.d_boxes, self.d_scores, self.d_classes, self.num_d],
+                feed_dict={self.image_tensor: img_expanded})
+        return boxes, scores, classes, num
+
+
+image_path = '../pklot-dataset-modified/test/images/10307.jpg'
+image = np.array(Image.open(image_path))
+
+detector = ParkingLotDetector()
+boxes, scores, classes, num = detector.get_classification(image)
+
+print(boxes)
+print(classes)
+print(scores)
+print(num)
